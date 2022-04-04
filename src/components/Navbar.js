3ad1a31228028a9fb364d34c3ad7e522cf049f12
NimajF <benjafran2000@gmail.com>
2022-04-04 23:52:17 +0200

    Add Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import { BsBag } from "react-icons/bs";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <span>Cakes and breakfast</span>
+      <BsBag />
+    </div>
+  );
+}
