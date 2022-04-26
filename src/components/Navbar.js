b67dd45966cd4a706085ada450899909cbf22d0d
NimajF <benjafran2000@gmail.com>
2022-04-26 23:36:53 +0200

    Add navbar icons and links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,35 @@
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 import { BsBag } from "react-icons/bs";
+import { FiEdit } from "react-icons/fi";
 
 export default function Navbar() {
+  const { data: session, status } = useSession();
+
   return (
-    <div className="navbar">
-      <span>Cakes and breakfast</span>
-      <BsBag />
-    </div>
+    <nav className="navbar">
+      <Link href="/">
+        <span>Cakes and breakfast</span>
+      </Link>
+      <div className="nav_links">
+        {session && (
+          <div>
+            <Link href="/api/auth/signout">
+              <p>Log out</p>
+            </Link>
+            <Link href="/create">
+              <span>
+                <FiEdit />
+              </span>
+            </Link>
+          </div>
+        )}
+        <Link href="/cart">
+          <span>
+            <BsBag />
+          </span>
+        </Link>
+      </div>
+    </nav>
   );
 }
