e88d2cb5c32cf62837041557d45bf25d76fd27f6
NimajF <benjafran2000@gmail.com>
2022-05-04 16:56:49 +0200

    Add icon categories and titles

--- src/components/ProductNav.js
+++ src/components/ProductNav.js
@@ -1,19 +1,26 @@
-import { GiStairsCake } from "react-icons/gi";
+import Link from "next/link";
+import { GiStairsCake, GiPresent } from "react-icons/gi";
 import { MdFreeBreakfast } from "react-icons/md";
 import { IoIceCreamSharp } from "react-icons/io5";
 import styles from "../styles/Home.module.css";
 
 const productIcons = [
-  <GiStairsCake />,
-  <MdFreeBreakfast />,
-  <IoIceCreamSharp />,
+  { icon: <GiStairsCake />, iconCategory: "tortas", title: "Tortas" },
+  { icon: <MdFreeBreakfast />, iconCategory: "desayunos", title: "Desayunos" },
+  { icon: <GiPresent />, iconCategory: "box", title: "Box" },
+  { icon: <IoIceCreamSharp />, iconCategory: "postres", title: "Postres" },
 ];
 
 export default function ProductNav() {
   return (
     <div className={styles.productNav}>
       {productIcons.map((icon, idx) => (
-        <div key={idx}>{icon}</div>
+        <Link key={idx} href={`/${icon.iconCategory}`}>
+          <div>
+            <span className={styles.icon}>{icon.icon}</span>
+            <p className={styles.iconTitle}>{icon.title}</p>
+          </div>
+        </Link>
       ))}
     </div>
   );
