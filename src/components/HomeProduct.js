165d2c1c5eb2eabf9ed5fbd5f863db492762a0eb
NimajF <benjafran2000@gmail.com>
2022-05-04 16:53:15 +0200

    Add edit icon and link

--- src/components/HomeProduct.js
+++ src/components/HomeProduct.js
@@ -1,11 +1,21 @@
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import { MdModeEdit } from "react-icons/md";
 
 export default function HomeProduct({ product }) {
+  const { data: session, status } = useSession();
   return (
     <div className={styles.homeProduct}>
       <Link href={`/product/${product._id}`}>
         <div>
+          {session && (
+            <Link href={`/edit/${product._id}`}>
+              <div>
+                <MdModeEdit />
+              </div>
+            </Link>
+          )}
           <h2>{product.title}</h2>
           <p>$ {product.price}</p>
         </div>
