f48ba942b47f810ea0d319a40f762dd641d91890
NimajF <benjafran2000@gmail.com>
2022-06-10 18:08:07 +0200

    Add cloud vars

--- next.config.js
+++ next.config.js
@@ -9,6 +9,8 @@ module.exports = {
   env: {
     CLOUD: process.env.CLOUD_URL,
     CLOUD_PRESET: process.env.CLOUD_PRESET_NAME,
+    CLOUD_KEY: process.env.CLOUD_API_KEY,
+    CLOUD_SECRET: process.env.CLOUD_API_SECRET,
   },
   images: {
     domains: ["res.cloudinary.com"],
