06b0ae7929413899c7192cd22093f4988df02f9e
NimajF <benjafran2000@gmail.com>
2022-12-16 21:47:25 +0100

    Add a festivity check feature

---
+++ src/utils/festivityCheck.js
@@ -0,0 +1,16 @@
+const festivities = {
+  pascuas: "Pascuas",
+  "san valentin": "San Valentín",
+  navidad: "Navidad",
+  "dia del padre": "Día del Padre",
+  "dia de la madre": "Día de la Madre",
+  "dia del nino": "Día del Niño",
+};
+
+export default function (productFestivity) {
+  const isProductFestivity =
+    productFestivity !== "no" ? productFestivity : false;
+  const festivity = festivities?.[isProductFestivity];
+
+  return festivity;
+}
