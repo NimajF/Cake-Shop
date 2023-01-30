46452b96e6f20d41b0025d9df62ea2179cebd445
NimajF <benjafran2000@gmail.com>
2023-01-30 14:02:38 -0300

    Add Festivity in view page and stuff

--- src/pages/product/[id].js
+++ src/pages/product/[id].js
@@ -1,18 +1,31 @@
 import { useState, useEffect, useContext } from "react";
+import { useSession } from "next-auth/react";
 import { DataContext } from "../../store/GlobalState";
 import Layout from "../../components/Layout";
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
+import ImageViewer from "../../components/ImageViewer";
+import festivityCheck from "../../utils/festivityCheck";
 import Cookies from "js-cookie";
+import { MdModeEdit } from "react-icons/md";
 import styles from "../../styles/Product.module.css";
 
 export default function DetailProduct({ product }) {
-  const { title, price, description, content, category, images } = product;
+  const { data: session } = useSession();
+  const [pr] = product;
+  const { title, price, description, content, category, images } = pr;
   const { addToCart } = useContext(DataContext);
+  const festivity = festivityCheck(pr.festivity);
+
+  const [viewImage, setViewImage] = useState(images[0].url);
 
   const handleSave = () => {
-    addToCart(product);
+    addToCart(pr);
+  };
+
+  const viewImg = (img) => {
+    setViewImage(img);
   };
 
   return (
@@ -25,20 +38,40 @@ export default function DetailProduct({ product }) {
           <div className={styles.productImages}>
             <div className={styles.imgDiv}>
               <Image
-                src={images[0].url}
+                src={viewImage}
                 height="100%"
                 width="100%"
                 layout="responsive"
                 objectFit="cover"
                 priority
+                alt={`Imagen del product ${title}`}
               />
             </div>
+            <ImageViewer images={images} selectImage={viewImg} />
           </div>
           <div className={styles.productDetails}>
             <h2>{title}</h2>
+
             <Link href={`/${category}`}>
-              <p>{category}</p>
+              <p className={styles.categoryLink}>
+                {category.charAt(0).toUpperCase() + category.slice(1)}
+              </p>
             </Link>
+            {session && (
+              <Link href={`/edit/${pr._id}`}>
+                <span className={styles.editSvg}>
+                  <p>
+                    Editar <MdModeEdit />
+                  </p>
+                </span>
+              </Link>
+            )}
+
+            {festivity ? (
+              <span className={styles.festivity}>Especial {festivity}</span>
+            ) : (
+              ""
+            )}
             <span className={styles.price}>$ {price}</span>
             <Link href="/cart">
               <button onClick={handleSave}>Añadir a la cesta</button>
