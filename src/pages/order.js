2fd4afe1fac6ba5782df5f2880014db0e8da1740
NimajF <benjafran2000@gmail.com>
2023-01-30 14:08:14 -0300

    Tweak order structure

--- src/pages/order.js
+++ src/pages/order.js
@@ -1,37 +1,56 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import { DataContext } from "../store/GlobalState";
-import { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
 import Layout from "../components/Layout";
+import Head from "next/head";
 import Link from "next/link";
 import emailjs from "emailjs-com";
 import { BsInstagram } from "react-icons/bs";
 import { AiOutlineWhatsApp } from "react-icons/ai";
+import { TiTick } from "react-icons/ti";
 import styles from "../styles/Cart.module.css";
+
+const useTotalPrice = () => {
+  const { cart } = useContext(DataContext);
+  return Object.values(cart).reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
+};
 
 export default function Order() {
   const { cart, resetCart, isCartEmpty } = useContext(DataContext);
+  const price = useTotalPrice();
   const cartObj = Object.values(cart);
 
   const { push } = useRouter();
-
-  const [order, setOrder] = useState(cartObj || []);
-  const [totalPrice, setTotalPrice] = useState(0);
+  const [order, setOrder] = useState(cartObj);
+  // const [totalPrice, setTotalPrice] = useState(0);
   const [orderMsg, setOrderMsg] = useState("");
+  const [msgSent, setMsgSent] = useState(false);
+  const [msgCopied, setCopy] = useState(false);
+  const msgRef = useRef(null);
+
+  useEffect(() => {
+    const item = JSON.parse(localStorage.getItem("cart"));
+    if (Object.keys(item).length === 0 && item.constructor === Object) {
+      push("/");
+    }
+  }, []);
 
   useEffect(() => {
     setOrder(cartObj);
   }, [JSON.stringify(cartObj)]);
 
   useEffect(() => {
-    let price = 0;
+    // let price = 0;
     let msg = [];
     for (let product of cartObj) {
-      price += product.currentPrice;
+      // price += product.currentPrice;
       msg.push(`${product.quantity} x ${product.title}`);
     }
-    setTotalPrice(price);
+    // setTotalPrice(price);
     setOrderMsg(msg.join(", "));
-    console.log(cart);
   }, [cart]);
 
   // const handleSubmit = async (e) => {
@@ -65,63 +84,128 @@ export default function Order() {
           console.log(error.text);
         }
       );
+    setMsgSent(true);
+    // resetCart();
+    // push("/");
+  };
+
+  const handleCopy = (e) => {
+    msgRef.current.select();
+    document.execCommand("copy");
+    e.target.focus();
+    setCopy(true);
+  };
+
+  const handleRedirect = () => {
     resetCart();
+    push("/");
   };
 
   return (
     <Layout>
+      <Head>
+        <title>Mi Orden</title>
+      </Head>
+      <div className={msgSent ? "modal show" : "modal"}>
+        <div className={"modal-span"}>
+          <span>
+            <h5>Mensaje enviado</h5>
+          </span>
+          <p>
+            Listo!! El correo fue enviado con éxito. Me pondré en contacto con
+            vos lo antes posible.
+          </p>
+          {/* <span>
+            <svg
+              className="checkmark"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 52 52"
+            ></svg>
+          </span> */}
+          <button onClick={handleRedirect}>Inicio</button>
+        </div>
+      </div>
       <h1>Casi listo...</h1>
       <h2>
-        Puedes enviarme el pedido via Whatsapp, Instagram o correo electronico
+        Podés enviarme el pedido via Whatsapp, Instagram o correo electrónico.
       </h2>
       <div className={styles.orderContainer}>
         <div className={styles.order}>
-          {order.map((obj) => (
-            <span key={obj.title}>
+          {order.map((obj, idx) => (
+            <span
+              key={obj.title}
+              className="productSpan"
+              style={{
+                borderBottom: `${
+                  idx !== order.length - 1 ? "1px solid #83838333" : ""
+                }`,
+              }}
+            >
               <p>
-                {obj.quantity} x {obj.title}
+                {obj.title} <br></br> <small>Cantidad: {obj.quantity}</small>
               </p>
-              <p>${obj.currentPrice}</p>
+              <p>${obj.price * obj.quantity}</p>
             </span>
           ))}
-          <span style={{ marginTop: "30px" }}>
+          <span style={{ marginTop: `${price > 0 ? "30px" : "0"}` }}>
             <b>Total</b>
-            <b>${totalPrice}</b>
+            <b>${price}</b>
           </span>
-          <div className={styles.emailDiv}>
-            <form onSubmit={sendEmail}>
-              <p>
-                <label htmlFor="name">Nombre</label>
-                <input type="text" name="name" />
-              </p>
-              <p>
-                <label htmlFor="email">Correo Electronico</label>
-                <input type="email" name="email" />
-              </p>
-              <p>
-                <label htmlFor="subject">Asunto</label>
-                <input type="text" name="subject" />
-              </p>
-              <p>
-                <label htmlFor="message">Mensaje</label>
-                <textarea name="message" defaultValue={orderMsg} />
-              </p>
-              <button type="submit">Enviar</button>
-            </form>
-          </div>
+        </div>
+        <div className={styles.emailDiv}>
+          <h3 className={styles.emailH3}>Enviame un mail</h3>
+          <form onSubmit={sendEmail}>
+            <p>
+              <label htmlFor="name">Nombre</label>
+              <input type="text" name="name" placeholder="Escribe tu nombre" />
+            </p>
+            <p>
+              <label htmlFor="email">Correo Electronico</label>
+              <input type="email" name="email" placeholder="Escribe tu email" />
+            </p>
+            <p>
+              <label htmlFor="subject">Asunto</label>
+              <input
+                type="text"
+                name="subject"
+                placeholder="Escribe un asunto"
+              />
+            </p>
+            <p>
+              <label htmlFor="message">Mensaje</label>
+              <textarea
+                name="message"
+                defaultValue={orderMsg}
+                ref={msgRef}
+                style={msgCopied ? { background: "#bcffbc" } : {}}
+              />
+              <span className={styles.suggestion}>
+                Podés usar este pedido autogenerado y terminar de
+                personalizarlo.
+              </span>
+              <span onClick={handleCopy}>Copiar mensaje</span>
+            </p>
+            <button type="submit">Enviar</button>
+          </form>
+          <h3 className={styles.emailH3} style={{ textAlign: "center" }}>
+            O enviame un mensaje
+          </h3>
           <div className={styles.orderLinks}>
-            <Link href={"/"}>
-              <button>
-                Envíar
-                <AiOutlineWhatsApp />
-              </button>
-            </Link>
-            <Link href={"/"}>
-              <button>
-                Envíar
-                <BsInstagram />
-              </button>
-            </Link>
+            <a
+              href="https://wa.link/2nbked"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Whatsapp
+            </a>
+
+            <a
+              href="https://instagram.com/dulcevictorinaok?igshid=YmMyMTA2M2Y="
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Instagram
+            </a>
           </div>
         </div>
       </div>
