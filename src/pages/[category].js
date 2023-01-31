25d5a1a6c604af13db434c5e65c513f3a2a34ecb
NimajF <benjafran2000@gmail.com>
2023-01-30 23:45:46 -0300

    Fix fetch api url

--- src/pages/[category].js
+++ src/pages/[category].js
@@ -85,7 +85,9 @@ export default function CategoryIndex({ products }) {
 }
 
 export const getServerSideProps = async ({ query: { category, sort } }) => {
-  const res = await fetch(`${process.env.BASE_URL}/${category}?sort=${sort}`);
+  const res = await fetch(
+    `${process.env.BASE_URL}/api/${category}?sort=${sort}`
+  );
   const products = await res.json();
   const categories = ["tortas", "box", "postres", "desayunos"];
   if (!categories.includes(category)) {
