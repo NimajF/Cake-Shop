3b127c374516d18230b046d9ab15eab72ef377af
NimajF <benjafran2000@gmail.com>
2023-01-31 00:40:42 -0300

    Fix config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = nextConfig;
 
 module.exports = {
   env: {
-    BASE_URL: "https://dulcevictorina.vercel.app",
+    BASE_URL: process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000",
     CLOUD: process.env.CLOUD_URL,
     CLOUD_PRESET: process.env.CLOUD_PRESET_NAME,
     CLOUD_KEY: process.env.CLOUD_API_KEY,
