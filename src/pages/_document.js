e4626b9586b51f0a5fefc98f063a109b0371701e
NimajF <benjafran2000@gmail.com>
2023-03-17 01:11:38 -0300

    Fix import

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import dulcevictorina from "../../public/";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
